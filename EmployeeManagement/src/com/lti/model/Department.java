de6ab1bf16d14d96cf9c771e23b50b3d29213ec2
Zack <kartikgarg938@gmail.com>
2020-10-15 13:26:00 +0530

    department added in sub-branch

---
+++ EmployeeManagement/src/com/lti/model/Department.java
@@ -0,0 +1,20 @@
+package com.lti.model;
+
+public class Department {
+int deptid;
+String depName;
+public int getDeptid() {
+	return deptid;
+}
+public void setDeptid(int deptid) {
+	this.deptid = deptid;
+}
+public String getDepName() {
+	return depName;
+}
+public void setDepName(String depName) {
+	this.depName = depName;
+}
+
+
+}
